e9287b043a3710f75de7bd4c254bfcdf4328623d
Lizziku <139032220+Lizziku@users.noreply.github.com>
2023-10-29 18:23:50 +0400

    firs commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// document.getElementById("imgx").display = none;
+
+// function openX() {
+//     document.getElementById("imgx").style.display = block;
+// }
+
+let square = getElementsByClassName("square");
+let imgx = getElementById("img");
+
+function openX() {
+    var imgx = document.getElementById("img");
+    if (img.style.display === "none") {
+        img.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
